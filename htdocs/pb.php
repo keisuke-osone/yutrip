bc4ffefbc3b61d0b92401a8f730dd814730ec799
Takashi Honda <takashi.0628.honda@gmail.com>
2014-02-10 23:37:09 +0900

    ファイル分割削除

--- htdocs/pb.php
+++ htdocs/pb.php
@@ -1,19 +1,18 @@
 <?php
-//API情報を取得する際の使い方
-require_once dirname(__FILE__) . '/bootstrap.php';
 
+require_once dirname(__FILE__) . '/../bootstrap.php';
+
+//本番用
+$geo = array(
+	'latitude' => $_REQUEST['latitude'],
+	'longitude' => $_REQUEST['longitude']
+);
 
 $geo = array(
 	'latitude' => '35.0116391',
 	'longitude' => '135.7680321'
 );
 
-//ぐるなびAPIの例
-// $api = \Classes\Api::getInstance('gnavi');
-// $api->setRange(500);
-// $result = $api->call($geo);
-
-//京都APIの例
 $api = \Classes\Api::getInstance('pb');
 $option['transportation'] = 'cycle';
 $api->setRange(500);
