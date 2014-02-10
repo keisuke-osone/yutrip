470cd7f05335cfdcd82b044bc645762934d2c7da
Takashi Honda <takashi.0628.honda@gmail.com>
2014-02-10 23:42:04 +0900

    gnavi, pb 結合

---
+++ htdocs/search.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once dirname(__FILE__) . '/../bootstrap.php';
+
+//本番用
+$geo = array(
+	'latitude' => $_REQUEST['latitude'],
+	'longitude' => $_REQUEST['longitude']
+);
+
+$geo = array(
+	'latitude' => '35.0116391',
+	'longitude' => '135.7680321'
+);
+
+$api = \Classes\Api::getInstance('gnavi');
+$api->setRange(500);
+$result['g'] = $api->call($geo);
+
+$api = \Classes\Api::getInstance('pb');
+$option['transportation'] = 'cycle';
+$api->setRange(500);
+$result['p'] = $api->call($geo, $option);
+
+echo json_encode($result);
