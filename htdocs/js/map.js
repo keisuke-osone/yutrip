1b56e9b3f7344d0424c078193e42080331b5ab90
Keisuke OSONE <paradi.bass@gmail.com>
2014-02-11 02:12:12 +0900

    formとか入れた

--- htdocs/js/map.js
+++ htdocs/js/map.js
@@ -1,7 +1,7 @@
 function initialize() {
   var mapOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 8,
+    center: new google.maps.LatLng(34.985458 , 135.757755),
+    zoom: 12,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map_canvas"),
