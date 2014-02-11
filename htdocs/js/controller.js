ff797358da39b606673f6ba3638efc3835819eb4
keisuke_osone <keisuke_osone@cybird.co.jp>
2014-02-11 16:27:35 +0900

    textbox内の情報取得

--- htdocs/js/controller.js
+++ htdocs/js/controller.js
@@ -7,6 +7,14 @@ main.initialize();
 
 //検索ボタンが押された際の処理
 d3.select('#search')
-	.on('click', function () {
-		console.log('押されたよ');
-	});
+    .on('click', function () {
+        console.log('押されたよ');
+        //テキストボックス内の情報取得
+        var inputText = d3.select('#search-text')[0][0].value;
+        
+        console.log(inputText);
+        // ここに入力の確認処理
+
+        // ここから文字列取得
+        // 
+    });
